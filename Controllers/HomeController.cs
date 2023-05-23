33c330a1d3f7dbf879da4387cfbfe030cacc9ff1
Alestin <itworkplace46@gmail.com>
2023-05-23 15:15:50 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using iText.Html2pdf;
+using Microsoft.AspNetCore.Components.RenderTree;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.CodeModifier.CodeChange;
+using NuGet.Protocol;
+using PageForMarkReport.Models;
+using System.Diagnostics;
+using System.Net;
+using ToAspNetProject;
+
+namespace PageForMarkReport.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Print()
+        {
+            var model = MarkReportPrintDto.Instanse;
+            // Handler for convert Html to Pdf
+            string html = await ConvertHtmlToString.RenderViewToStringAsync("Print", model, ControllerContext);
+
+            using var file = new FileStream("output.pdf", FileMode.Create);
+
+            HtmlConverter.ConvertToPdf(html, file);
+            return View(file);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PageForMarkReport.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
